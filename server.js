9528423193bff1e9cad2cb31c6ac2fb292259d34
nekacurry <anneka498@gmail.com>
2022-05-11 15:24:44 -0500

    users can submit and save posts

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
+// Set db
+require('./data/reddit-db');
+require('./controllers/posts')(app);
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get("/posts/new", (req, res) => {
+  res.render("posts-new");
+});
+
+
 app.listen(3000);
+
