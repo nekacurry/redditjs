a0fccfc157c6803dea5f610d8b5ecf2522ad2c2a
nekacurry <anneka498@gmail.com>
2022-05-11 14:46:34 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
